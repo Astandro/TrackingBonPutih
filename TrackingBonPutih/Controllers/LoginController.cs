264f8a01c4e524a16ed2d43c9343faba5f9ff176
aliarham11 <aliarham.skom@gmail.com>
2014-07-03 13:58:19 +0700

    Login and Validation success

--- TrackingBonPutih/Controllers/LoginController.cs
+++ TrackingBonPutih/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrackingBonPutih.Models;
 
 namespace TrackingBonPutih.Controllers
 {
@@ -10,10 +11,32 @@ namespace TrackingBonPutih.Controllers
     {
         //
         // GET: /Login/
+        
+        
 
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(user checkLogin)
+        {
+            trackingEntities context = new trackingEntities();
+            if (ModelState.IsValid)
+            {
+                IEnumerable<user> result = from b in context.users
+                                           where b.NPK == checkLogin.NPK && b.PASS == checkLogin.PASS
+                                           select b;
+            
+                if(result.Count() > 0)
+                    return View("../Home/Index");
+                else
+                    ModelState.AddModelError("", "username / password salah");
+            
+            }
+            
+            return View(checkLogin);
         }
 
     }
